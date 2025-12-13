a7ffb094af4797337ac0a1ccadb55033b372b5d4
sameer73-s <sameer778880@gmail.com>
2025-12-13 22:43:59 +0300

    Initial commit: Flutter Shop Task Application - Clean architecture implementation - Domain layer with product and review entities

---
+++ lib/Feature/prodects/domain/entities/product_entities.dart
@@ -0,0 +1,19 @@
+import 'package:shop_task/Feature/prodects/domain/entities/reviews_entities.dart';
+
+class ProductEntities {
+  final String? title;
+  final String? shippingInformation;
+  final double? price;
+  final double? rating;
+  final List<Reviews>? reviews;
+  final List<String>? images;
+
+  ProductEntities({
+    required this.title,
+    required this.shippingInformation,
+    required this.price,
+    required this.rating,
+    required this.reviews,
+    required this.images,
+  });
+}
---
+++ lib/Feature/prodects/domain/entities/reviews_entities.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Reviews {
+  int? rating;
+  String? comment;
+  String? date;
+  String? reviewerName;
+  String? reviewerEmail;
+
+  Reviews({
+    required this.rating,
+    required this.comment,
+    required this.date,
+    required this.reviewerName,
+    required this.reviewerEmail,
+  });
+}
