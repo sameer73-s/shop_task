f98971a55db67a1ea7526e9bf5d8b9ca855ea7b9
sameer73-s <sameer778880@gmail.com>
2025-12-14 00:36:24 +0300

    get product usecase

---
+++ lib/Feature/prodects/domain/UseCases/get_product.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:shop_task/Feature/prodects/domain/entities/product_entity.dart';
+import 'package:shop_task/Feature/prodects/domain/reposotories/product_reposotory.dart';
+import 'package:shop_task/core/errors/failure.dart';
+
+class GetProduct {
+  final ProductReposotory reposotory;
+
+  GetProduct({required this.reposotory});
+  Future<Either<Failure, ProductEntity>> call() {
+    return reposotory.getproducts();
+  }
+}
