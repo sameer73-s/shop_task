347af37919117f9ea7aa96f18384256699879b93
sameer73-s <sameer778880@gmail.com>
2025-12-14 21:57:09 +0300

    product repo impl

---
+++ lib/Feature/prodects/data/reposotries/product_reposotiry_implementation.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:shop_task/Feature/prodects/data/datasources/product_local_datasource.dart';
+import 'package:shop_task/Feature/prodects/data/datasources/product_remote_datasource.dart';
+import 'package:shop_task/Feature/prodects/domain/entities/product_entity.dart';
+import 'package:shop_task/Feature/prodects/domain/reposotories/product_reposotory.dart';
+import 'package:shop_task/core/connection/network_info.dart';
+import 'package:shop_task/core/errors/expentions.dart';
+import 'package:shop_task/core/errors/failure.dart';
+import 'package:shop_task/core/params/params.dart';
+
+class ProductReposotiryImplementation extends ProductReposotory {
+  final NetworkInfo networkInfo;
+  final ProductRemoteDatasource remoteDatasource;
+  final ProductLocalDatasource localDatasource;
+
+  ProductReposotiryImplementation({
+    required this.networkInfo,
+    required this.remoteDatasource,
+    required this.localDatasource,
+  });
+
+  @override
+  Future<Either<Failure, ProductEntity>> getproducts({
+    required productsParams params,
+  }) async {
+    if (await networkInfo.isConnected!) {
+      try {
+        final remoteproduct = await remoteDatasource.getproduct(params);
+        localDatasource.cacheProduct(remoteproduct);
+        return right(remoteproduct);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {}
+
+    try {
+      final localproduct = await localDatasource.getLastProduct();
+      return right(localproduct);
+    } on CacheExeption catch (e) {
+      return Left(Failure(errMessage: e.errorMessage));
+    }
+  }
+}
